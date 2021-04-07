0c7e9ab7676871af0b6e3c2da7126b97152c695e
Yuzuru Utsunomiya <35525121+yuzuruu@users.noreply.github.com>
2021-04-07 21:44:12 +0900

    first commit

---
+++ tutorial_fy2021.r
@@ -0,0 +1,11 @@
+# いじっておぼえるR
+# 7th. April. 2021
+# Yuzuru Utsunomiya, Ph. D.
+
+# tidyverseパッケージをインストール
+# 1回インストールするだけでよい。起動するたびにしなくてもよい
+install.packages("tidyverse")
+
+# tidyverseパッケージを読み込む
+# 起動するたびにする。
+library(tidyverse)
