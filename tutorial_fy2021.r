7339a6328724aa1ccb46157e9befaae04cfe823f
Yuzuru Utsunomiya <35525121+yuzuruu@users.noreply.github.com>
2021-04-07 21:58:49 +0900

    アヤメのデータを使った散布図書き方と宿題を書き足した。

--- tutorial_fy2021.r
+++ tutorial_fy2021.r
@@ -19,3 +19,33 @@
 # 起動するたびにする。
 library(tidyverse)
 
+# irisデータで散布図を書いてみる
+# irisデータは、統計解析パッケージのお勉強に最もよく使われるデータ
+# アヤメの花のパーツ寸法に関するデータ
+# オブジェクトに図を代入
+iris_scatterplot <- 
+  # データをパイプで送り込む
+  iris %>% 
+  # 作図領域をつくる
+  ggplot2::ggplot(
+    data = ., 
+    aes(
+      x = Sepal.Length, # x軸に使う変数
+      y = Petal.Length, # y軸に使う変数
+      color = Species   # 塗り分けに使う変数
+      )
+    ) +
+  # 散布図を描く。
+  geom_point() + 
+  # テーマを適用する。
+  theme_classic()
+
+# オブジェクトを表示する
+iris_scatterplot
+
+# 宿題
+# https://kazutan.github.io/fukuokaR11/intro_ggplot2.html
+# を眺めて、いろいろ設定をいじって挙動を確認する。
+# 結果をggplot2::ggsave()関数を使ってpng形式にて保存、Google chat上に投稿。
+# メインタイトルに自分の名前をローマ字で記入すること。
+
