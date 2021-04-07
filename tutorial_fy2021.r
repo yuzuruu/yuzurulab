4d1e4867ebc243dbc3d71fc69778777b672761ec
Yuzuru Utsunomiya <35525121+yuzuruu@users.noreply.github.com>
2021-04-07 21:50:22 +0900

    added comments on frequently-used keybinds

--- tutorial_fy2021.r
+++ tutorial_fy2021.r
@@ -1,11 +1,21 @@
+###################################################################
 # いじっておぼえるR
 # 7th. April. 2021
 # Yuzuru Utsunomiya, Ph. D.
+#
+# よく使うキーバインド
+# Ctrl+Enter:選択部分を実行
+# Ctrl+s:上書き保存
+# Ctrl+Shift+c:選択部分をコメントアウト
+# 
+# くわしいことはHelp -> Cheetsheetsからチートシートをダウンロード
 
 # tidyverseパッケージをインストール
 # 1回インストールするだけでよい。起動するたびにしなくてもよい
-install.packages("tidyverse")
+# インストールさえしてしまえばコメントアウトしてもよい。
+# install.packages("tidyverse")
 
 # tidyverseパッケージを読み込む
 # 起動するたびにする。
 library(tidyverse)
+
